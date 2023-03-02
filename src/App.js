75292634e1acea1b1d2e1dddf7a90cbb095d8fce
olgakahraman <ol.khrmn@gmail.com>
2023-03-01 21:56:06 -0500

    building search engine

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
-
+import Dictionary from "./Dictionary";
 import './App.css';
 
  export default function App() {
   return (
     <div className="App">
+      <div className="container">
       <header className="App-header">
-        
+       
       
       </header>
+
+      <main>
+        <Dictionary />
+      </main>
+
+
+      <footer className = "App-footer"><small>Coded by Olga D Kahraman</small></footer>
+      </div>
     </div>
   );
 }
