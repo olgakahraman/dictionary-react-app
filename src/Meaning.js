9f59b848c0be1861a1c48ebf676ac52ccfae6be4
olgakahraman <ol.khrmn@gmail.com>
2023-03-06 22:20:05 -0500

    api integration

--- src/Meaning.js
+++ src/Meaning.js
@@ -1,13 +1,21 @@
 import React from "react";
 
 export default function Meaning(props) {
+  console.log(props.meaning);
   return (
     <div className="Meaning">
       <h3>{props.meaning.partOfSpeech}</h3>
-    <em>{props.meaning.definition}</em>
-    <p>{props.meaning.example}</p>
+      {props.meaning.definitions.map(function (definition, index) {
+        return (
+          <div key={index}>
+            <p>
+              {definition.definition}
+              <br />
+              <em>{definition.example}</em>
+            </p>
           </div>
-   
-   
+        );
+      })}
+    </div>
   );
 }
