54a651279bf068817347ba888e48c3dd3fddbcb0
olgakahraman <ol.khrmn@gmail.com>
2023-03-07 22:36:12 -0500

    added synonym component

--- src/Meaning.js
+++ src/Meaning.js
@@ -1,21 +1,18 @@
+
 import React from "react";
+import Synonyms from "./Synonyms";
 
 export default function Meaning(props) {
-  console.log(props.meaning);
   return (
     <div className="Meaning">
       <h3>{props.meaning.partOfSpeech}</h3>
-      {props.meaning.definitions.map(function (definition, index) {
-        return (
-          <div key={index}>
-            <p>
-              {definition.definition}
-              <br />
-              <em>{definition.example}</em>
-            </p>
-          </div>
-        );
-      })}
+      <p>
+        <strong>Definition: </strong>{props.meaning.definition}
+        <br />
+        <strong>Example: </strong><em>{props.meaning.example}</em>
+        <br />
+        <Synonyms synonyms={props.meaning.synonyms} />
+      </p>
     </div>
   );
 }
