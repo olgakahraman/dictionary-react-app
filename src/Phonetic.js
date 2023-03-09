3e4d9dcd6fcb2dce73f07099bdecb8e798f95c24
olgakahraman <ol.khrmn@gmail.com>
2023-03-08 22:34:44 -0500

    added phonetics and listen button

---
+++ src/Phonetic.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Phonetic.css";
+
+export default function Phonetic (props) {
+   const audio = new Audio(props.phonetic.audio);
+   function playSound() {
+     audio.play();
+   }
+    return (
+      <div className="Phonetic">
+        <i className="fas fa-volume-up" onClick={playSound}></i>
+        <span className="text"> {props.phonetic.text} </span>
+      </div>
+    );
+}
