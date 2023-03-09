d2239461fead0f4fe0ab394c3e5b237c58ea6642
olgakahraman <ol.khrmn@gmail.com>
2023-03-08 22:34:02 -0500

    Update Meaning.js

--- src/Meaning.js
+++ src/Meaning.js
@@ -2,7 +2,7 @@ import React from "react";
 import Synonyms from "./Synonyms";
 
 export default function Meaning(props) {
-  console.log(props.meaning);
+
   return (
     <div className="Meaning">
       <h3>{props.meaning.partOfSpeech}</h3>
@@ -12,10 +12,10 @@ export default function Meaning(props) {
             <p>
               <strong>Definition:</strong> {definition.definition}
               <br />
-              <strong>Example: </strong>
-              <em>{definition.example}</em>
+              <strong>Example:</strong>
+              <em>{definition.example}</em> </p>
               <Synonyms synonyms={definition.synonyms} />
-            </p>
+           
           </div>
         );
       })}
