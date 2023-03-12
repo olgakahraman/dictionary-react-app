1937f468771ca8238d52dcfc3ccfd176086544d1
olgakahraman <ol.khrmn@gmail.com>
2023-03-12 00:03:33 -0500

    Update Dictionary.js

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -8,18 +8,16 @@ export default function Dictionary(props) {
   let [results, setResults] = useState(null);
   let [loaded, setLoaded] =useState(false);
 
-  function handleResponse(response) {
-    
+  function handleResponse(response) { 
     setResults(response.data[0]);
   }
 
   function search() {
-    
-
     // documentation: https://dictionaryapi.dev/e
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
     axios.get(apiUrl).then(handleResponse);
   }
+
 function handleSubmit(event){
   event.preventDefault();
   search();
@@ -41,7 +39,7 @@ if (loaded){
       <h1 className="text-center m-5">DICTIONARY</h1>
       <section>
       <h3>What word would you like to look up?</h3>
-      <form onSubmit={search}>
+      <form onSubmit={handleSubmit}>
         <input type="search" onChange={handleKeywordChange} defaultValue={props.defaultKeyword}/>
       </form>
       <div className="hint">suggested words: sunset, wine, yoga, plant...</div>
