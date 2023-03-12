91a39fe49655e001403c2db825014916224ac10a
olgakahraman <ol.khrmn@gmail.com>
2023-03-11 21:43:20 -0500

    Update Meaning.js

--- src/Meaning.js
+++ src/Meaning.js
@@ -6,19 +6,11 @@ export default function Meaning(props) {
   return (
     <div className="Meaning">
       <h3>{props.meaning.partOfSpeech}</h3>
-      {props.meaning.definitions.map(function (definition, index) {
-        return (
-          <div key={index}>
-            <p>
-              <strong>Definition:</strong> {definition.definition}
-              <br />
-              <strong>Example:</strong>
-              <em>{definition.example}</em> </p>
-              <Synonyms synonyms={definition.synonyms} />
+     <p>{props.meaning.definitions[0].definition}</p>
+     <p>{props.meaning.definitions[0].example}</p>
+              <Synonyms synonyms={props.meaning.synonyms} />
            
-          </div>
-        );
-      })}
+        
     </div>
   );
 }
