b5f28f778989d9ee3539c3f419919fc6f3017d72
olgakahraman <ol.khrmn@gmail.com>
2023-03-13 00:00:17 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
 import Dictionary from "./Dictionary";
-import './App.css';
+import "./App.css";
 
- export default function App() {
+export default function App() {
   return (
     <div className="App">
       <div className="container">
-      <header className="App-header">
-       
-      
-      </header>
+        <header className="App-header"></header>
 
-      <main>
-        <Dictionary defaultKeyword="sunset" />
-      </main>
+        <main>
+          <Dictionary defaultKeyword="countryside" />
+        </main>
 
-
-      <footer className = "App-footer"><small>Coded by Olga Dietiuk Kahraman</small></footer>
+        <footer className="App-footer">
+          <small>
+            This project was coded by Olga Dietiuk Kahraman - 2023 and it is{" "}
+            <a
+              href="https://github.com/olgakahraman/dictionary-react-app"
+              target="_blank"
+              title="GitHub"
+            >
+              open-sourced on GitHub
+            </a>
+          </small>
+        </footer>
       </div>
     </div>
   );
 }
-
-
