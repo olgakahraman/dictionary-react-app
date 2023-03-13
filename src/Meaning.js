8c01fecc6d065552e5d449b1a27149c5bc66f703
olgakahraman <ol.khrmn@gmail.com>
2023-03-13 00:42:15 -0400

    trying to deploy

--- src/Meaning.js
+++ src/Meaning.js
@@ -3,15 +3,12 @@ import Synonyms from "./Synonyms";
 import "./Meaning.css";
 
 export default function Meaning(props) {
-
   return (
     <div className="Meaning">
       <h3>{props.meaning.partOfSpeech}</h3>
-     <p>{props.meaning.definitions[0].definition}</p>
-     <div className="example">{props.meaning.definitions[0].example}</div>
-              <Synonyms synonyms={props.meaning.synonyms} />
-           
-        
+      <p>{props.meaning.definitions[0].definition}</p>
+      <div className="example">{props.meaning.definitions[0].example}</div>
+      <Synonyms synonyms={props.meaning.synonyms} />
     </div>
   );
 }
--- src/Results.js
+++ src/Results.js
@@ -8,15 +8,15 @@ export default function Results(props) {
     return (
       <div className="Results">
         <section>
-        <h2>{props.results.word}</h2>
-        {props.results.phonetics.map(function (phonetic, index) {
-          return (
-            <div key={index}>
-              <Phonetic phonetic={phonetic} />
-            </div>
-          );
-        })}
-</section>
+          <h2>{props.results.word}</h2>
+          {props.results.phonetics.map(function (phonetic, index) {
+            return (
+              <div key={index}>
+                <Phonetic phonetic={phonetic} />
+              </div>
+            );
+          })}
+        </section>
 
         {props.results.meanings.map(function (meaning, index) {
           return (
